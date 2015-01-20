d3dac7942706df5623d06406cf4ff8ac0fc93c46
naza <naza@naza.mikronika.com.pl>
2015-01-20 08:34:18 +0100

    Custom bindings.

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,56 +1,77 @@
 (function () {
 
-	/* scope building */
-	var scopeManager = new SimpleBinder.modules.scopes.ScopeManager();
-	SimpleBinder.scopeManager = scopeManager;
-	scopeManager.build();
-	/* scope building end */
+    /* scope building */
+    var scopeManager = new SimpleBinder.modules.scopes.ScopeManager();
+    SimpleBinder.scopeManager = scopeManager;
+    scopeManager.build();
+    /* scope building end */
 
-	scopeManager.setElementScopes('[simple-repeat]');
-	(function () {
-		var nodes = document.querySelectorAll("[simple-repeat]"),
-				length = nodes.length, x = 0, node;
+    scopeManager.setElementScopes('[simple-repeat]');
+    (function () {
+        var nodes = document.querySelectorAll("[simple-repeat]"),
+                length = nodes.length, x = 0, node;
 
-		binders = [];
+        binders = [];
 
-		var id = 0, Repeat = SimpleBinder.modules.binders.Repeat;
-		for (; x < length; x += 1) {
-			node = nodes[x];
+        var id = 0, Repeat = SimpleBinder.modules.binders.Repeat;
+        for (; x < length; x += 1) {
+            node = nodes[x];
 
-			node.$binding.$scope.rows = [];
-			for (var i = 0; i < 20; i += 1) {
-				node.$binding.$scope.rows.push({
-					id: i,
-					name: 'test item : ' + i
-				});
-			}
+            node.$binding.$scope.rows = [];
+            for (var i = 0; i < 20; i += 1) {
+                node.$binding.$scope.rows.push({
+                    id: i,
+                    name: 'test item : ' + i
+                });
+            }
 
-			binders.push(new Repeat(node));
+            binders.push(new Repeat(node));
 
-		}
-	})();
-
-
-	scopeManager.setElementScopes('[simple-bind]');
-	(function () {
-		var nodes = document.querySelectorAll("[simple-bind]"),
-				length = nodes.length, x = 0, node;
-
-		binders = [];
-
-		var id = 0, SimpleBind = SimpleBinder.modules.binders.SimpleBind;
-		for (; x < length; x += 1) {
-
-			node = nodes[x];
-			node.$binding.$scope['test' + id] = "Wartość [" + id + "] = jakas";
-			id += 1;
-			if (id > 1) {
-				id = 0;
-			}
+        }
+    })();
 
 
-			binders.push(new SimpleBind(node));
+    scopeManager.setElementScopes('[simple-bind]');
+    (function () {
+        var nodes = document.querySelectorAll("[simple-bind]"),
+                length = nodes.length, x = 0, node;
 
-		}
-	})();
+        binders = [];
+
+        var id = 0, SimpleBind = SimpleBinder.modules.binders.SimpleBind;
+        for (; x < length; x += 1) {
+
+            node = nodes[x];
+            node.$binding.$scope['test' + id] = "Wartość [" + id + "] = jakas";
+            id += 1;
+            if (id > 1) {
+                id = 0;
+            }
+
+
+            binders.push(new SimpleBind(node));
+
+        }
+    })();
+
+    SimpleBinder.customBind('stooltip', function ($element, $scope) {
+        $($element).tooltip({
+            title: $scope.$id,
+            placement: 'right'
+        });
+    });
+
+    scopeManager.setElementScopes('[stooltip]');
+    (function () {
+        var nodes = document.querySelectorAll("[stooltip]"),
+                length = nodes.length, x = 0, node,
+                constructor = SimpleBinder.customBinders['stooltip'].fn;
+        for (; x < length; x += 1) {
+            node = nodes[x];
+            binders.push(new constructor(node, node.$binding.$scope));
+        }
+    })();
+
+
+
 })();
--- public_html/js/src/SimpleBinder.js
+++ public_html/js/src/SimpleBinder.js
@@ -1,22 +1,31 @@
 var SimpleBinder;
 (function () {
-	SimpleBinder = {
-		modules: {
-			utils: {},
-			binders: {},
-			scopes: {}
-		},
-		module: function (moduleName) {
-			var path = moduleName.split('.'), x = 0, length = path.length, module,
-					current = SimpleBinder.modules;
-			for (; x < length; x += 1) {
-				module = path[x];
-				if (typeof current[module] === 'undefined') {
-					current[module] = {};
-				}
-				current = current[module];
-			}
-			return current;
-		}
-	};
+    SimpleBinder = {
+        customBinders: {},
+        modules: {
+            utils: {},
+            binders: {},
+            scopes: {}
+        },
+        module: function (moduleName) {
+            var path = moduleName.split('.'), x = 0, length = path.length, module,
+                    current = SimpleBinder.modules;
+            for (; x < length; x += 1) {
+                module = path[x];
+                if (typeof current[module] === 'undefined') {
+                    current[module] = {};
+                }
+                current = current[module];
+            }
+            return current;
+        },
+        customBind: function () {
+            var name = arguments[0],
+                    constructor = arguments[arguments.length - 1];
+            SimpleBinder.customBinders[name] = {
+                name: name,
+                fn: constructor
+            };
+        }
+    };
 })();
